418f183d18c6d371c74fde63b4fd94fd68b821a0
Israel Fleetwood <67342332+hgsisrael@users.noreply.github.com>
2021-07-06 18:22:59 +0000

    almost done

--- script.js
+++ script.js
@@ -1,31 +1,106 @@
 // Author:
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
 
 // Global ML variables
+let featureExtractor;
+let imgFeatures;
+let knnClassifier;
+let video;
+let isModelReady;
+let ups;
+let downs;
+let lefts;
+let rights;
 //let featureExtractor;
 
 function setup() {
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP ("Model loading, please wait...");
+  textP.parent(textDiv);
+  textP2 = createP("[Training data here.]");
+  textP2.parent(textDiv);
 
+  buildButtons();
+
+  ups = 0;
+  downs = 0;
+  lefts = 0;
+  rights = 0;
+
+  video = createCapture(VIDEO, videoReady);
 }
 
 function draw() {
-
+  if(isModelReady) {
+    translate(width, 0);
+    scale(-1, 1);
+    image(video, 0, 0);
+    imgFeatures = featureExtractor.infer(canvas);
+    if(knnClassifier.getNumLabels()> 0 ) {
+      knnClassifier.classify(imgFeatures, gotResults);
+    }
+  }
 }
 
 function buildButtons() {
+  buttonDiv = createDiv();
+  upButton = createButton("Up");
+  upButton.parent(buttonDiv);
+  upButton.mousePressed(function() {
+    ups++;
+    textP.html("Ups: " + ups + " - Downs:" + downs + " - Lefts: " + lefts + " - Rights:" + rights);
+    knnClassifier.addExample(imgFeatures, "Down");
+  })
 
-}
+   downButton = createButton("Down");
+  downButton.parent(buttonDiv);
+  downButton.mousePressed(function() {
+    downs++;
+    textP.html("Ups: " + ups + " - Downs:" + downs + " - Lefts: " + lefts + " - Rights:" + rights);
+    knnClassifier.addExample(imgFeatures, "Down");
+  })
+
+   leftButton = createButton("Left");
+  leftButton.parent(buttonDiv);
+  leftButton.mousePressed(function() {
+    lefts++;
+    textP.html("Ups: " + ups + " - Downs:" + downs + " - Lefts: " + lefts + " - Rights:" + rights);
+    knnClassifier.addExample(imgFeatures, "Left");
+  })
+
+  rightButton = createButton("Right");
+  rightButton.parent(buttonDiv);
+  rightButton.mousePressed(function() {
+    rights++;
+    textP.html("Ups: " + ups + " - Downs:" + downs + " - Lefts: " + lefts + " - Rights:" + rights);
+    knnClassifier.addExample(imgFeatures, "Right");
+
+  buttonDiv.style("display", mome)
+} 
+
 
 function videoReady() {
-
+  video.style("display", "none");
+  featureExtractor = ml5.featureExtractor("MobileNet", featureExtractorLoaded);
 }
 
 function featureExtractorLoaded() {
-
+  isModelReady = true;
+  knnClassifier = KNNClassifier();
+  textP.html("Begin posing and adding data!");
+  buttonDiv.style("display", "block");
 }
 
-function gotResults(error, results) {
 
+function gotResults(error, results) {
+  if(error) {
+    console
+  }
 }
